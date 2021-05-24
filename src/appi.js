07f4028f8e799e15060942a4566dde07ff523cbb
shashwat <shashwat8828@mail.com>
2021-05-24 18:21:47 +0530

    heroku website hosting

---
+++ src/appi.js
@@ -0,0 +1,23 @@
+const path=require("path")
+const express = require("express");
+const ap = express();
+const port = 80;
+//console.log(__dirname);
+
+
+
+const staticpath = path.join(__dirname,"../public");//path to public folder which constins statics files
+
+
+
+ap.use(express.static(staticpath));//buildin middileware
+
+
+
+ap.listen(port,()=>{
+    console.log(`this app started successfully on port ${port}`)
+    });
+
+
+
+
